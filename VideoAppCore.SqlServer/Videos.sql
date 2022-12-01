562b6ac7b5ef88738c06bcbba21be1dc6ec02ccc
Ray <dlghdnd@gmail.com>
2022-12-01 16:41:51 +0900

    비디오 앱을 위한 Videos 테이블 생성 및 로컬 DB에 게시

---
+++ VideoAppCore.SqlServer/Videos.sql
@@ -0,0 +1,13 @@
+﻿-- 비디오 테이블
+CREATE TABLE [dbo].[Videos]
+(
+	[Id] INT NOT NULL Identity(1,1) PRIMARY KEY,
+	[Created] DateTimeOffset(7) Default(SysDateTimeOffset() AT TIME ZONE 'Korea Standard Time'),
+	
+	[Title] NVarChar(255) Not Null,			-- 제목
+	[Url] NVarChar(Max) Null,				-- URL
+
+	[Name] NVarChar(50) Null,				-- 이름
+	[Company] NVarChar(255) Null,			-- 회사
+)
+Go
